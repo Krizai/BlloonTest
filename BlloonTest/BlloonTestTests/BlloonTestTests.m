f2643c5980dbfea144bbaba88f26e88866a2e007
Dmitry Povolotsky <krizai@gmail.com>
2014-09-15 00:00:49 +0300

    Added several tests

--- BlloonTest/BlloonTestTests/BlloonTestTests.m
+++ BlloonTest/BlloonTestTests/BlloonTestTests.m
@@ -7,8 +7,13 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "DPBook.h"
 
 @interface BlloonTestTests : XCTestCase
+
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 
 @end
 
@@ -17,7 +22,19 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"blloon" withExtension:@"momd"];
+    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    
+    //store
+    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"test.sqlite"];
+    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:_managedObjectModel];
+    [_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:nil];
+    
+    //object context
+    _managedObjectContext = [[NSManagedObjectContext alloc] init];
+    [_managedObjectContext setPersistentStoreCoordinator:_persistentStoreCoordinator];
+    
 }
 
 - (void)tearDown
@@ -26,9 +43,43 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testBookCreation
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    DPBook* book = [DPBook bookFromJSONDictionary:nil inContext:self.managedObjectContext];
+    XCTAssert(book != nil, @"Book creation failed (nil dictionary )");
+    XCTAssert(book.title == nil, @"Book creation failed (nil dictionary )");
+    XCTAssert(book.author == nil, @"Book creation failed (nil dictionary )");
+    XCTAssert(book.thumbnailUrl == nil, @"Book creation failed (nil dictionary)");
+    
+    book = [DPBook bookFromJSONDictionary:[NSDictionary dictionary] inContext:self.managedObjectContext];
+    XCTAssert(book != nil, @"Book creation failed (empty dictionary )");
+    XCTAssert(book.title == nil, @"Book creation failed (empty dictionary )");
+    XCTAssert(book.author == nil, @"Book creation failed (empty dictionary )");
+    XCTAssert(book.thumbnailUrl == nil, @"Book creation failed (empty dictionary)");
+    
+    NSDictionary* dictionary = @{@"title": @"test1",
+                                 @"author": @"test2",
+                                 @"payout": @"test3"};
+    book = [DPBook bookFromJSONDictionary:dictionary inContext:self.managedObjectContext];
+    XCTAssert(book != nil, @"Book creation failed (title and author)");
+    XCTAssert([book.title isEqualToString:@"test1"], @"Book creation failed (title and author)");
+    XCTAssert([book.author isEqualToString:@"test2"], @"Book creation failed (title and author)");
+    XCTAssert(book.thumbnailUrl == nil, @"Book creation failed (title and author)");
+    
+    dictionary = @{@"title": @"test1",
+                   @"author": @"test2",
+                   @"small_cover_image_url": @"test3"};
+    book = [DPBook bookFromJSONDictionary:dictionary inContext:self.managedObjectContext];
+    XCTAssert(book != nil, @"Book creation failed (title and author and thumbnailUrl)");
+    XCTAssert([book.title isEqualToString:@"test1"], @"Book creation failed (title and author and thumbnailUrl)");
+    XCTAssert([book.author isEqualToString:@"test2"], @"Book creation failed (title and author and thumbnailUrl)");
+    XCTAssert([book.thumbnailUrl isEqualToString:@"test3"], @"Book creation failed (title and author and thumbnailUrl)");
+    
 }
+    
+    - (NSURL *)applicationDocumentsDirectory
+    {
+        return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+    }
 
 @end
